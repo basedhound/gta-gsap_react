30e7261b3176ffb334ecfd67967d183f08303c21
fvukelic <franck.vukelic@gmail.com>
2025-06-18 17:29:01 +0700

    Add FirstVideo.jsx, Build Layout, Animate

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+
 import NavBar from "./components/NavBar";
 import Hero from "./sections/Hero";
+import FirstVideo from "./sections/FirstVideo";
 
 gsap.registerPlugin(ScrollTrigger);
 function App() {
   return (
     <main>
       <NavBar />
-      <Hero/>
+      <Hero />
+      <FirstVideo />
     </main>
   );
 }
