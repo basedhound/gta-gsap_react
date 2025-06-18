783f7e0c1bf9647a0b5dce4e0298e47de4c3b4d4
fvukelic <franck.vukelic@gmail.com>
2025-06-18 18:21:13 +0700

    Add Lucia part.1 before animations

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import NavBar from "./components/NavBar";
 import Hero from "./sections/Hero";
 import FirstVideo from "./sections/FirstVideo";
 import Jason from "./sections/Jason";
+import SecondVideo from "./sections/SecondVideo";
+import Lucia from "./sections/Lucia";
 
 gsap.registerPlugin(ScrollTrigger);
 function App() {
@@ -14,6 +16,8 @@ function App() {
       <Hero />
       <FirstVideo />
       <Jason />
+      <SecondVideo/>
+      <Lucia/>
     </main>
   );
 }
