13322421ee067574b4f652c1085850a3681880aa
fvukelic <franck.vukelic@gmail.com>
2025-06-19 23:39:10 +0700

    Back to previous constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,24 +1,14 @@
 import { useMediaQuery } from "react-responsive";
 
 export const useMaskSettings = () => {
-  const isSmallMobile = useMediaQuery({ maxWidth: 480 });
-  const isMobile = useMediaQuery({ minWidth: 481, maxWidth: 768 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
   const isTablet = useMediaQuery({ minWidth: 769, maxWidth: 1024 });
 
-  if (isSmallMobile) {
+  if (isMobile) {
     return {
       initialMaskPos: "50% -1500vh",
       initialMaskSize: "3100% 3100%",
       maskPos: "50% 7vh",
-      maskSize: "50% 50%",
-    };
-  }
-
-  if (isMobile) {
-    return {
-      initialMaskPos: "50% -1000rem",
-      initialMaskSize: "4500% 4500%",
-      maskPos: "50% 0rem",
       maskSize: "50% 50%",
     };
   }
@@ -32,7 +22,6 @@ export const useMaskSettings = () => {
     };
   }
 
-  // Desktop fallback
   return {
     initialMaskPos: "50% 22%",
     initialMaskSize: "3500% 3500%",
